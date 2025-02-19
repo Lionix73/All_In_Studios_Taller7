07906105777cf25525760956c45478b96cbfd704
Alejandro <alejolr1218@gmail.com>
2025-02-18 21:57:15 -0500

    Bug Enemy Spawner Fix

--- VJ_Taller7/Assets/Scripts/Enemies/NewEnemies/EnemySpawner.cs
+++ VJ_Taller7/Assets/Scripts/Enemies/NewEnemies/EnemySpawner.cs
@@ -33,6 +33,8 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
+        navMeshTriangulation = NavMesh.CalculateTriangulation();
+
         if(storeInitialPos){
             StoreInitialPositions();
         }
