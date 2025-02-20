089a04d6a0d637cf0b048494638938e81847edce
Villegazs <alejitov214@hotmail.com>
2025-02-19 19:30:56 -0500

    PopUp Implemented in all weapons

--- VJ_Taller7/Assets/Scripts/Guns/GunPickeable.cs
+++ VJ_Taller7/Assets/Scripts/Guns/GunPickeable.cs
@@ -6,8 +6,11 @@ public class GunPickeable : MonoBehaviour
 {
     [SerializeField] private GunType gunType;
     [SerializeField] private GameObject PickeableUI;
-    [SerializeField] private TextMeshProUGUI gunName;
     [SerializeField] private ObjectLookAtCamera lookCamera;
+    [SerializeField] private TextMeshProUGUI gunNameText;
+    [SerializeField] private TextMeshProUGUI damageText;
+    [SerializeField] private TextMeshProUGUI fireRateText;
+    [SerializeField] private TextMeshProUGUI magazineText;
 
     private Vector3 spinDirection = new Vector3(0, 1, 0);
     private GunManager gunManager;
@@ -15,8 +18,9 @@ public class GunPickeable : MonoBehaviour
 
     private void Awake() {
         //gunName.SetText(gunType.ToString() + "\n" + "Press E to pick up");
-        gunName.SetText(gunType.ToString());
+        gunNameText.SetText(gunType.ToString());
         PickeableUI.SetActive(false);
+
     }
 
     private void Update() {
@@ -27,11 +31,14 @@ public class GunPickeable : MonoBehaviour
         if (other.CompareTag("Player")) {
             gunManager = other.gameObject.GetComponentInChildren<GunManager>();
             gunManager.EnterPickeableGun(gunType);
-
+            GunScriptableObject gun = gunManager.GetGun(gunType); //Pa tomar lo que quiran del arma a pickear
+            damageText.text = $"DMG: {gun.Damage}";
+            fireRateText.text = $"Fire Rate: {gun.ShootConfig.FireRate}";
+            magazineText.text = $"Magazine: {gun.MagazineSize}";
             PickeableUI.SetActive(true);
             lookCamera = PickeableUI.GetComponentInChildren<ObjectLookAtCamera>();
 
-            GunScriptableObject gun = gunManager.GetGun(gunType); //Pa tomar lo que quiran del arma a pickear
+            
         }
     }
 
