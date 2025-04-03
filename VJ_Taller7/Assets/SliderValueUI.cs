11913d65c573ec0372bd8a74de774ab888dd39d5
santiCG <santiagocardenasgomez@gmail.com>
2025-04-02 19:05:35 -0500

    .

---
+++ VJ_Taller7/Assets/SliderValueUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SliderValueUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI valueText;
+    private Slider slider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateSlideNumber()
+    {
+        valueText.text = Mathf.FloorToInt(slider.value).ToString();
+    }
+}
