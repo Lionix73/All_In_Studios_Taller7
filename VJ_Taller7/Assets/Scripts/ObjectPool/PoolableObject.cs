87452ec1913acf0497454652b9a82ce6a4581ea1
Alejandro <alejolr1218@gmail.com>
2025-04-28 11:01:35 -0500

    All Enemyes with Art (WIP Elephant)

--- VJ_Taller7/Assets/Scripts/ObjectPool/PoolableObject.cs
+++ VJ_Taller7/Assets/Scripts/ObjectPool/PoolableObject.cs
@@ -6,8 +6,11 @@ public class PoolableObject : MonoBehaviour
 
     public virtual void OnDisable()
     {
+        //Debug.Log("OnDisable: " + gameObject.name);
+
         if (Parent != null)
         {
+            //Debug.Log("Returning to pool: " + gameObject.name);
             Parent.ReturnObjectToPool(this);
         }
     }
